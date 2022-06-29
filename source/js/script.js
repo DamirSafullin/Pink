609f1782adc0323d43dae69ee141ad63f06ad7c7
damur <damirsaf123@gmail.com>
2022-06-29 14:16:37 +0700

    added cross for burger menu

--- source/js/script.js
+++ source/js/script.js
@@ -5,4 +5,6 @@ let wrapper = document.querySelector('.main-nav-wrapper')
 button.addEventListener('click', function(evt) {
   menu.classList.toggle('menu-open')
   wrapper.classList.toggle('wrapper-open')
+  button.classList.toggle('main-nav__burger')
+  button.classList.toggle('main-nav__cross')
 })
